4ba315b18f35d31356d208888cb0543cff71e1d0
santithe78-alt <santithe78@gmail.com>
2026-01-06 17:41:43 +0000

    First day

---
+++ script.js
@@ -0,0 +1,32 @@
+console.log("hello")
+
+const alpha = ["a", "b", "c", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+
+function encrypt() {
+console.log("Encrypt")
+// get message text vaule
+document.getElementById(messageInput)
+//Get key vaule
+
+//create empty result string
+
+//look throught message characters
+
+    //Get the current character(.CharAt(i))
+
+    // Convert to lowercase(.toLowerCase())
+
+    //Convert to a number (alpha.indexOf(currentChar))
+
+    //Apply formula 
+
+    //Convert new char number into a character alpha[encryptedVaule]
+
+    //Add-encrypted character to a result string
+
+//Set encrypted message result text on screen
+}
+
+function decrypt() {
+    console.log("Decrypt")
+}
