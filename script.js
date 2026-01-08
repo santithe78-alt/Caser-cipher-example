35b24ff35cf7130b34452a96b01a4677663bdfd3
santithe78-alt <santithe78@gmail.com>
2026-01-08 17:13:52 +0000

    Second day

--- script.js
+++ script.js
@@ -3,28 +3,33 @@ console.log("hello")
 const alpha = ["a", "b", "c", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 
 function encrypt() {
-console.log("Encrypt")
-// get message text vaule
-document.getElementById(messageInput)
-//Get key vaule
+    console.log("Encrypt")
+    // get message text value
+    let msg = document.getElementById("messageInput").value;
+    console.log(msg);
+    //Get key value
+    let keyValue = document.getElementById("key").value
+    console.log(keyValue);
+    //create empty result string
+    let resultString = "";
 
-//create empty result string
+    //look throught message characters
 
-//look throught message characters
+        //Get the current character(.CharAt(i))
+        for (let i = 0; i < msg.length; i++) {
+            let char = msg.charAt()
+        }
+        // Convert to lowercase(.toLowerCase())
 
-    //Get the current character(.CharAt(i))
+        //Convert to a number (alpha.indexOf(currentChar))
 
-    // Convert to lowercase(.toLowerCase())
+        //Apply formula 
 
-    //Convert to a number (alpha.indexOf(currentChar))
+        //Convert new char number into a character alpha[encryptedVaule]
 
-    //Apply formula 
+        //Add-encrypted character to a result string
 
-    //Convert new char number into a character alpha[encryptedVaule]
-
-    //Add-encrypted character to a result string
-
-//Set encrypted message result text on screen
+    //Set encrypted message result text on screen
 }
 
 function decrypt() {
